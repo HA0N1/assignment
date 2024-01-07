2c704ed46eb2264962370ab742bb6edceeb1f80c
HA0N1 <intor48@gmail.com>
2024-01-07 21:01:38 +0900

    검색기능 구현. css 해야함

--- myjs.js
+++ myjs.js
@@ -35,18 +35,45 @@ fetch(
                         `;
 
       // api에 연동된 영화들 추가
-      $("#movies").append(cardTemp);
+      let movieApi = $(document.getElementById("movies"));
+      movieApi.append(cardTemp);
+      // $("#movies").append(cardTemp);
+
       // 이미지 클릭시 아이디 나오게 하기
-      $(`#${id} img`).click(function () {
+      $(`#${id} img`).click(() => {
         alert(`영화 ID: ${id}`);
       });
+      // $(`#${id} img`).click(function () {
+      //   alert(`영화 ID: ${id}`);
+      // });
     }
+    // 검색 기능 구현
+    let inputElement = $(document.querySelector("input"));
+    let mouseDown = document.querySelector("button");
+    function search() {
+      let inputText = inputElement.val().toLowerCase(); //input(This)를 val를 넣어주고 소문자 처리 해버리기
+      $(".movie-card").hide(); // 엔터를 눌렀을때 모든 영화 카드를 숨김
+      results.forEach((movie) => {
+        let title = movie["title"].toLowerCase();
+        if (title.includes(inputText)) {
+          $(`#${movie["id"]}`).show(); // 입력한 값이 title에 포함되어 있으면 show해주기.
+        }
+      });
+    }
+    mouseDown.addEventListener("click", search);
     // enter는 13번임. keyup은 눌렀다 땠을때
-    $("input").keyup(function (enter) {
+    inputElement.keyup(function press(enter) {
       if (enter.keyCode == 13) {
-        $("btn").click();
+        search();
+        // alert("안녕");
       }
-      // 단어검색
-      // for문으로 글자 뽑고 or 써서 title 글자랑 비교하기?
     });
   });
+// for (let i = 0; i < results.length; i++) {
+//   let movie = results[i];
+//   let title = movie["title"].toLowerCase();
+//   //includes: 배열의 항목에 특정 값이 포함되어 있는지를 판단 T/F 값으로 반환.
+//   if (title.includes(inputText)) {
+//     $(`#${movie["id"]}`).show(); // 입력한 값이 title에 포함되어 있으면 show해주기.
+//   }
+// }
